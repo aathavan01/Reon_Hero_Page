278c46e21010ae47c5cd514f910d5692be01b121
AATHAV <aathavan4580@gmail.com>
2022-09-15 12:10:33 +0530

    landing soon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import React from "react";
 
 function App() {
   return (
@@ -6,29 +6,9 @@ function App() {
       {/* <!-- Navbar Start --> */}
       <div className="container-fluid position-relative nav-bar p-0 dark sticky">
         <div className="position-relative " style={{ zIndex: "9" }}>
-          <nav className="navbar navbar-expand-lg bg-secondary navbar-dark py-3 py-lg-0 pl-3 pl-lg-5">
-            <a href="" className="navbar-brand">
-              <h1 className="yellowColor mb-1">
-                REON{" "}
-                <span className=" text-light mb-1 pl-5 pl-5">
-               {" "}
-                  <img
-                    style={{ height: "70px", width: "70px" }}
-                    src="/img/racing.png"
-                    alt="tyres"
-                  />
-                  <img
-                    style={{ height: "70px", width: "70px" }}
-                    src="/img/racing.png"
-                    alt="tyres"
-                  />
-                  <img
-                    style={{ height: "70px", width: "70px" }}
-                    src="/img/racing.png"
-                    alt="tyres"
-                  />
-                </span>
-              </h1>{" "}
+          <nav className="navbar navbar-expand-lg dark navbar-dark py-3 py-lg-0 pl-3 pl-lg-5 fixed-top">
+            <a href="/" className="navbar-brand">
+              <h1 className="yellowColor mb-1">REON </h1>{" "}
             </a>
             <button
               type="button"
@@ -43,7 +23,7 @@ function App() {
               id="navbarCollapse"
             >
               <div className="navbar-nav ml-auto py-0">
-                <a href="index.html" className="nav-item nav-link active">
+                <a href="/" className="nav-item nav-link active">
                   Home
                 </a>
                 <a href="#01" className="nav-item nav-link">
@@ -68,7 +48,7 @@ function App() {
       {/* <!-- Carousel Start --> */}
       <div
         className="container-fluid p-0 dark"
-        // style={{ marginBottom: "90px" }} 
+        // style={{ marginBottom: "90px" }}
       >
         <div
           id="header-carousel"
@@ -77,17 +57,19 @@ function App() {
         >
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img className="w-100" src="img/carousel-1.jpg" alt="Image" />
+              <img className="w-100 " src="img/carousel-1.jpg" alt="Vehicle" />
               <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-                <div className="p-3" style={{ maxWidth: "900px" }}>
-                  <h4 className="display-1 text-white mb-md-4">REON</h4>
-                  <h1 className="text-white mb-md-3">
+                <div style={{ maxWidth: "900px" }}>
+                  <h4 className="display-1 blueColor mb-md-4 fontFamily">
+                    REON
+                  </h4>
+                  <h1 className="text-white mb-md-5">
                     connects vehicle renters with nearby vehicle rental
                     providers
                   </h1>
                   <a
-                    href=""
-                    className="btn yellow font-weight-bold font py-md-2 px-md-3 mt-2"
+                    href="vehicles"
+                    className="btn yellow font-weight-bold font"
                   >
                     Hire Now
                   </a>
@@ -95,16 +77,18 @@ function App() {
               </div>
             </div>
             <div className="carousel-item">
-              <img className="w-100" src="img/carousel-2.jpg" alt="Image" />
+              <img className="w-100" src="img/carousel-2.jpg" alt="Vehicle" />
               <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div className="p-3" style={{ maxWidth: "900px" }}>
-                  <h4 className="display-1 text-white mb-md-4">REON</h4>
+                  <h4 className="display-1 blueColor mb-md-4 fontFamily">
+                    REON
+                  </h4>
                   <h1 className="text-white mb-md-3">
                     Consumers can find and rent any type of vehicle through
                     REON.
                   </h1>
                   <a
-                    href=""
+                    href="/vehicles"
                     className="btn yellow font-weight-bold font py-md-2 px-md-3 mt-2"
                   >
                     Hire Now
@@ -113,16 +97,18 @@ function App() {
               </div>
             </div>
             <div className="carousel-item">
-              <img className="w-100" src="img/carousel-2.jpg" alt="Image" />
+              <img className="w-100" src="img/carousel-2.jpg" alt="Vehicle" />
               <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                 <div className="p-3" style={{ maxWidth: "900px" }}>
-                  <h4 className="display-1 text-white mb-md-4">REON</h4>
+                  <h4 className="display-1 blueColor mb-md-4 fontFamily">
+                    REON
+                  </h4>
                   <h1 className="text-white mb-md-3">
                     Vehicle rental providers have wider customer reach through
                     REON.
                   </h1>
                   <a
-                    href=""
+                    href="vehicles"
                     className="btn yellow font-weight-bold font py-md-2 px-md-3 mt-2"
                   >
                     Hire Now
@@ -135,66 +121,46 @@ function App() {
             className="carousel-control-prev"
             href="#header-carousel"
             data-slide="prev"
-          >
-            <div
-              className="btn btn-dark"
-              style={{ width: "45px", height: "45px" }}
-            >
-              <span className="carousel-control-prev-icon mb-n2"></span>
-            </div>
-          </a>
+          >.</a>
           <a
             className="carousel-control-next"
             href="#header-carousel"
             data-slide="next"
-          >
-            <div
-              className="btn btn-dark"
-              style={{ width: "45px", height: "45px" }}
-            >
-              <span className="carousel-control-next-icon mb-n2"></span>
-            </div>
-          </a>
+          >.</a>
         </div>
       </div>
       {/* <!-- Carousel End --> */}
 
       {/* <!-- About Start --> */}
-      <div className="container-fluid pb-5 dark">
-        <div className="container pt-5 pb-3">
-          <h1 className="display-1 text-light yellowColor
-           text-center" id="01">
+      <div className="container-fluid pb-5 dark py-5" id="01">
+        <div className="container pt-5">
+          <h1
+            className="display-1 yellowColor p-
+           text-center"
+          >
             01
           </h1>
-          <h1 className="display-4 yellowColor text-center mb-5">
-            Welcome To <span className="blueColor">REON</span>
+          <h1 className="display-4 blueColor text-center mb-5">
+            Welcome To REON 
           </h1>
           <div className="row justify-content-center">
             <div className="col-lg-10 text-center">
               <img className="w-75 mb-4" src="img/about.png" alt="" />
-              <p className="text-light">
-                Justo et eos et ut takimata sed sadipscing dolore lorem, et
-                elitr labore labore voluptua no rebum sed, stet voluptua amet
-                sed elitr ea dolor dolores no clita. Dolores diam magna clita ea
-                eos amet, amet rebum voluptua vero vero sed clita accusam
-                takimata. Nonumy labore ipsum sea voluptua sea eos sit justo, no
-                ipsum sanctus sanctus no et no ipsum amet, tempor labore est
-                labore no. Eos diam eirmod lorem ut eirmod, ipsum diam
-                sadipscing stet dolores elitr elitr eirmod dolore. Magna elitr
-                accusam takimata labore, et at erat eirmod consetetur tempor
-                eirmod invidunt est, ipsum nonumy at et.
-              </p>
+              <h3 className="text-light">
+                {" "}
+                REON the people who connects the customers with vehicle rentals.
+              </h3>
             </div>
           </div>
           <div className="row mt-3">
             <div className="col-lg-4 mb-2">
               <div
-                className="d-flex align-items-center bg-secondary p-4 mb-4"
-                style={{ height: "150px" }}
+                className="d-flex align-items-center blue p-4 mb-4"
+                style={{ height: "120px" }}
               >
                 <div
                   className="d-flex align-items-center justify-content-center flex-shrink-0 yellow ml-n4 mr-4"
-                  style={{ width: "100px", height: "100px" }}
+                  style={{ width: "80px", height: "80px" }}
                 >
                   <i className="fa fa-2x fa-headset text-secondary "></i>
                 </div>
@@ -205,33 +171,33 @@ function App() {
             </div>
             <div className="col-lg-4 mb-2">
               <div
-                className="d-flex align-items-center bg-secondary p-4 mb-4"
-                style={{ height: "150px" }}
+                className="d-flex align-items-center blue p-4 mb-4"
+                style={{ height: "120px" }}
               >
                 <div
                   className="d-flex align-items-center justify-content-center flex-shrink-0 yellow ml-n4 mr-4"
-                  style={{ width: "100px", height: "100px" }}
+                  style={{ width: "80px", height: "80px" }}
                 >
                   <i className="fa fa-2x fa-car text-secondary"></i>
                 </div>
                 <h4 className="text-light text-uppercase m-0">
-                  Lots of vehicles
+                  Different types of vehicles
                 </h4>
               </div>
             </div>
             <div className="col-lg-4 mb-2">
               <div
-                className="d-flex align-items-center bg-secondary p-4 mb-4"
-                style={{ height: "150px" }}
+                className="d-flex align-items-center blue p-4 mb-4"
+                style={{ height: "120px" }}
               >
                 <div
                   className="d-flex align-items-center justify-content-center flex-shrink-0 yellow ml-n4 mr-4"
-                  style={{ width: "100px", height: "100px" }}
+                  style={{ width: "80px", height: "80px" }}
                 >
                   <i className="fa fa-2x fa-map-marker-alt text-secondary"></i>
                 </div>
                 <h4 className="text-uppercase m-0 text-light">
-                  Lots Of Locations
+                  The vehicle you need is near your location
                 </h4>
               </div>
             </div>
@@ -241,51 +207,84 @@ function App() {
       {/* <!-- About End --> */}
 
       {/* <!-- Services Start -->? */}
-      <div className="container-fluid py-5 dark">
-        <div className="container pt-5 pb-3">
-          <h1 className="display-1 yellowColor text-center" id="02">
-            02
-          </h1>
+      <div className="container-fluid  dark py-5" id="02">
+        <div className="container pb-3">
+          <h1 className="display-1 yellowColor text-center">02</h1>
           <h1 className="display-4 text-uppercase text-center mb-5 blueColor">
             Our Services
           </h1>
-          <div class="row mb-5">
-            <div class="col-lg-4 mb-4 mb-lg-0">
-              <div class="step">
+          <div className="row mb-5">
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <div className="step">
                 <span>1</span>
-                <div class="step-inner">
-                  <span class="number yellowColor">01.</span>
-                  <h3>Select a car</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Vero, laboriosam!
-                  </p>
+                <div className="step-inner">
+                  {/* <h1 className="number yellowColor">01.</h1> */}
+                  <h2 className="blueColor">
+                    Reon offers all kinds of vehicles for rent to customers
+                  </h2>
+                  <h3 className="text-white">
+                    *Reon offers customers all types of vehicles on rent based
+                    on category. Eg:-
+                    <br />
+                    <br />
+                    1.Farming
+                    <br />
+                    2.Construction
+                    <br />
+                    3.Tour
+                    <br />
+                    4.Transportation
+                    <br />
+                    5.School Services or long term rentals(Subscription based)
+                    <br />
+                    6.Events
+                  </h3>
                 </div>
               </div>
             </div>
-            <div class="col-lg-4 mb-4 mb-lg-0">
-              <div class="step">
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <div className="step">
                 <span>2</span>
-                <div class="step-inner">
-                  <span class="number yellowColor">02.</span>
-                  <h3>Fill up form</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Vero, laboriosam!
-                  </p>
+                <div className="step-inner">
+                  {/* <h1 className="number yellowColor">02.</h1> */}
+                  <h2 className="blueColor">
+                    Reon offers a location filter feature
+                  </h2>
+                  <h3 className="text-white">
+                    *customers can rent a nearby vehicle by mentioning the
+                    required location and searching.
+                    <br />
+                    <br />
+                    *This will save the fuel cost for the vehicle owners and
+                    will bring the vehicle to your destination in a short time.
+                    <br />
+                    Thus there is a high chance of getting a vehicle rental at
+                    an affordable price.
+                  </h3>
                 </div>
               </div>
             </div>
-            <div class="col-lg-4 mb-4 mb-lg-0">
-              <div class="step">
+            <div className="col-lg-4 mb-4 mb-lg-0">
+              <div className="step">
                 <span>3</span>
-                <div class="step-inner">
-                  <span class="number yellowColor">03.</span>
-                  <h3>Payment</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Vero, laboriosam!
-                  </p>
+                <div className="step-inner">
+                  {/* <h1 className="number yellowColor">03.</h1> */}
+                  <h2 className="blueColor">
+                    Reon offers a wider opportunities to vehicle owners
+                  </h2>
+                  <h3 className="text-white">
+                    *Vehicle owners can add vehicles to reon web app and get
+                    more customers with reon
+                    <br />
+                    <br />
+                    *Vehicle owners who have bought a new vehicle can reach
+                    easily
+                    <br /> and get more customers from reon.
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                  </h3>
                 </div>
               </div>
             </div>
@@ -295,11 +294,9 @@ function App() {
       {/* <!-- Services End --> */}
 
       {/* <!-- Banner Start --> */}
-      <div className="container-fluid py-5 dark">
-        <div className="container py-5">
-          <h1 className="display-1 yellowColor  text-center" id="03">
-            03
-          </h1>
+      <div className="container-fluid dark pt-5" id="03">
+        <div className="container">
+          <h1 className="display-1 yellowColor  text-center">03</h1>
           <h1 className="display-4 text-uppercase text-center mb-5 blueColor">
             JOIN
           </h1>
@@ -307,9 +304,9 @@ function App() {
             <div className="py-5">
               <h1 className="display-1 yellowColor mb-4">Join With Us</h1>
               <h1 className="text-uppercase text-light mb-4">
-                If you wish to provide vehicle rental services with us !  
+                If you wish to provide vehicle rental services with us !
               </h1>
-              <a className="btn yellow mt-2 py-2 px-3 font" href="">
+              <a className="btn yellow mt-2 py-2 px-3 font" href="register">
                 Register Now
               </a>
             </div>
@@ -319,11 +316,9 @@ function App() {
       {/* <!-- Banner End --> */}
 
       {/* <!-- Contact Start --> */}
-      <div className="container-fluid py-5 dark">
-        <div className="container pt-5 pb-3">
-          <h1 className="display-1 yellowColor text-center" id="04">
-            04
-          </h1>
+      <div className="container-fluid py-5 dark" id="04">
+        <div className="container pb-3 pt-4">
+          <h1 className="display-1 yellowColor text-center">04</h1>
           <h1 className="display-4 text-uppercase text-center mb-5 blueColor">
             Contact Us
           </h1>
@@ -369,7 +364,10 @@ function App() {
                     ></textarea>
                   </div>
                   <div>
-                    <button className="btn yellow py-3 px-5" type="submit">
+                    <button
+                      className="btn text-dark yellow py-3 px-5"
+                      type="submit"
+                    >
                       Send Message
                     </button>
                   </div>
@@ -378,7 +376,7 @@ function App() {
             </div>
             <div className="col-lg-5 mb-2">
               <div
-                className="bg-secondary d-flex flex-column justify-content-center px-5 mb-4"
+                className="lightDark d-flex flex-column justify-content-center px-5 mb-4"
                 style={{ height: "435px" }}
               >
                 <div className="d-flex mb-3">
@@ -406,10 +404,7 @@ function App() {
                   <i className="fa fa-2x fa-headset yellowColor flex-shrink-0 mr-3"></i>
                   <div className="mt-n1">
                     <h5 className="text-light">Customer Services</h5>
-                    <p className="m-0">
-                      +94761903375 or <br />
-                      0212345678
-                    </p>
+                    <p className="m-0">+94761903375 or 0212345678</p>
                   </div>
                 </div>
               </div>
@@ -420,7 +415,7 @@ function App() {
       {/* <!-- Contact End -->  */}
 
       {/* <!-- Back to Top --> */}
-      <a href="#" className="btn btn-lg yellow btn-lg-square back-to-top">
+      <a href="/" className="btn btn-lg yellow btn-lg-square back-to-top">
         <i className="fa fa-angle-double-up"></i>
       </a>
     </>
